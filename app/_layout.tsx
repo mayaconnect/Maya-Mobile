18e517b20ab5ac8a2c2ee1a4792adb911f7914b0
glafay <guillaumel1103@gmail.com>
2026-01-27 11:47:01 +0100

    push

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,9 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { LoadingScreen } from '@/components/common/loading-screen';
+import { AppProvider } from '@/contexts/app-context';
 import { AuthProvider, useAuth } from '@/hooks/use-auth';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import { AppProvider } from '@/contexts/app-context';
 import { offlineSync } from '@/utils/offline-sync';
 import { Ionicons } from '@expo/vector-icons';
 import { useFonts } from 'expo-font';
@@ -23,15 +23,15 @@ function RootLayoutNav() {
 
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="onboarding/onboarding-2" options={{ headerShown: false }} />
-      <Stack.Screen name="onboarding/onboarding-3" options={{ headerShown: false }} />
-      <Stack.Screen name="onboarding/onboarding-4" options={{ headerShown: false }} />
-      <Stack.Screen name="connexion/login" options={{ headerShown: false }} />
-      <Stack.Screen name="connexion/signup" options={{ headerShown: false }} />
-      <Stack.Screen name="connexion/forgot-password" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack.Screen name="index" options={{ headerShown: false , animation: 'fade', animationDuration: 25 }} />
+      <Stack.Screen name="onboarding/onboarding-2" options={{ headerShown: false , animation: 'fade', animationDuration: 25 }} />
+      <Stack.Screen name="onboarding/onboarding-3" options={{ headerShown: false , animation: 'fade', animationDuration: 25 }} />
+      <Stack.Screen name="onboarding/onboarding-4" options={{ headerShown: false , animation: 'fade', animationDuration: 25 }} />
+      <Stack.Screen name="connexion/login" options={{ headerShown: false , animation: 'fade', animationDuration: 25 }} />
+      <Stack.Screen name="connexion/signup" options={{ headerShown: false , animation: 'fade', animationDuration: 25 }} />
+      <Stack.Screen name="connexion/forgot-password" options={{ headerShown: false , animation: 'fade', animationDuration: 25 }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false , animation: 'fade', animationDuration: 25 }} />
+      <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' , animation: 'fade', animationDuration: 25 }} />
     </Stack>
   );
 }
